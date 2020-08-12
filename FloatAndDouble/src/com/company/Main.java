c4a6be90a19ce2855a0f9f200ed3cbb168d6f67d
TXBIT <shenlong203@gmail.com>
2020-08-13 02:33:43 +0700

    05-011-float-and-double-Primitive-Types

---
+++ FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum Value = " + myMinFloatValue);
+        System.out.println("Float Maximum Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum Value = " + myMinDoubleValue);
+        System.out.println("Double Maximum Value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+//        float myFloatValue = (float) 5.25;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+    }
+}
