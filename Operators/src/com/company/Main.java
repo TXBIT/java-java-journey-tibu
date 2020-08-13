de9fd88c273fa2b31686d71ce8c96b31775b19a6
TXBIT <shenlong203@gmail.com>
2020-08-13 14:18:12 +0700

    05 022 Operator Precedence

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -87,5 +87,29 @@ public class Main {
         if (wasCar) {
             System.out.println("wasCar is true");
         }
+
+        // 1. Create a double variable with a value of 20.00
+        // 2. Create a second variable of type double with the value of 80.00
+        // 3. Add both numbers together and multiply by 100.00
+        // 4. Use the remainder operator to figure out what the remainder from the result of the operation in step 3
+        // and 40.00. The modulus and remainder operator can be used on int as well as double
+        // 5. Create a boolean variable that assigns the value true if the remainder in #4 is 0, or false if it's not 0
+        // 6. Output the boolean variable
+        // 7. Write an if-then statement that displays a message "Got some remainder" if the boolean in step 5 is not
+        // true
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("myValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        // boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        boolean isNoRemainder = theRemainder == 0;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+
     }
 }
