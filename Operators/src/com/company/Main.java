9414dfa58b2d68d644eed6dc81ac4d4fc20b1181
TXBIT <shenlong203@gmail.com>
2020-08-13 11:51:21 +0700

    05 021 Ternary Operator

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -81,5 +81,11 @@ public class Main {
         if (isCar) {
             System.out.println("This will not be printed");
         }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
     }
 }
